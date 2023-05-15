6875206cc4dae920e5513c97f341ad8d9dba4b28
jaimeBalseiro <jaime.diaz0303@gmail.com>
2023-05-14 21:23:06 -0400

    done

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers(){
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+      let isPrime = true;
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) count++;
+    }
+    return count;
+}
+
+
+const t0 = performance.now();
+
+setTimeout(() => {
+for (let index = 0; index < 100; index++) {
+    countPrimeNumbers();
+}
+    
+    const t1 = performance.now();
+    console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`);
+}, 0);
